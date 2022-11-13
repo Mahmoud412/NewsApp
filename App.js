a5ebd8bf3b31394ef8b4af094235e9d3885061d2
Mahmoud412 <mahmoudashour219@gmail.com>
2022-11-13 17:01:35 +0200

    init commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { SafeAreaView , Text} from "react-native";
+
+const App= () => {
+
+
+  
+  return (
+    <SafeAreaView>
+
+    <Text>test</Text>
+     
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
